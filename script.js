9134b473c4c151086b9ccfaf382a4ee461e7c421
Vishal Pawar <vishalpawar425@gmail.com>
2022-03-16 22:38:25 +0530

    updated the proxy
    Using proxy to stop the cors error

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener('load', ()=> {
             long = position.coords.longitude;
             lat = position.coords.latitude;
             const proxy = "https://cors-anywhere.herokuapp.com/";
-            const key = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`
+            const key = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`
             // console.log(key);
             fetch(key).then(response => {
                 return response.json()
